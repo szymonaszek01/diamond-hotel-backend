5cc951ede945f7125d86cc7d68a2699d993e8efb
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-11 00:31:03 +0200

    initial commit

---
+++ src/main/java/com/app/diamondhotelbackend/DiamondHotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.diamondhotelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondHotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondHotelBackendApplication.class, args);
+	}
+
+}

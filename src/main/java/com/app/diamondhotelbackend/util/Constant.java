d395761b9438b6890eb75e003bfa87e0a5bd6e9f
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-11 21:59:06 +0200

    task-005 Adding util class to store all project's constants

---
+++ src/main/java/com/app/diamondhotelbackend/util/Constant.java
@@ -0,0 +1,15 @@
+package com.app.diamondhotelbackend.util;
+
+public class Constant {
+    public static final String USER = "USER";
+
+    public static final String ADMIN = "ADMIN";
+
+    public static final String APPROVED = "APPROVED";
+
+    public static final String WAITING_FOR_PAYMENT = "WAITING_FOR_PAYMENT";
+
+    public static final String CANCELLED = "CANCELLED";
+
+    public static final String USER_PROFILE_NOT_FOUND = "User profile not found";
+}

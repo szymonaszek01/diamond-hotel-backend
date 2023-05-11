a44d04697d86dcef44f0230e28390d4bfe9f80c7
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-11 22:18:30 +0200

    task-009 Adding JSON Web Token service

---
+++ src/main/java/com/app/diamondhotelbackend/service/JwtService.java
@@ -0,0 +1,70 @@
+package com.app.diamondhotelbackend.service;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+@Slf4j
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "secret123412398";
+
+    public String createJwt(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("authorities", List.of(userDetails.getAuthorities()));
+
+        return Jwts.builder().setClaims(claims).setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)).signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Boolean validateToken(String token, UserDetails userProfileDetails) {
+        try {
+            final String username = extractUsername(token);
+            return (username.equals(userProfileDetails.getUsername()) && !isTokenExpired(token));
+
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: {}" + e.getMessage());
+            return false;
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: {}" + e.getMessage());
+            return false;
+        } catch (ExpiredJwtException e) {
+            log.error("JWT token is expired: {}" + e.getMessage());
+            return false;
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT token is unsupported: {}" + e.getMessage());
+            return false;
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}" + e.getMessage());
+            return false;
+        }
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+}

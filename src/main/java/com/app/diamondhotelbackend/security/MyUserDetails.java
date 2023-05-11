3640a4f325879d1b13bb569d851d40b88b37650f
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-11 22:34:51 +0200

    task-011 Adding custom "User details" which will be saved in JSON Web Token

---
+++ src/main/java/com/app/diamondhotelbackend/security/MyUserDetails.java
@@ -0,0 +1,61 @@
+package com.app.diamondhotelbackend.security;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+public class MyUserDetails implements UserDetails {
+
+    private long id;
+
+    private String email;
+
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public MyUserDetails(long id, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

fb1616ab79f7ff150fa8f9b7a42777962c9c6667
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-11 22:30:49 +0200

    task-010 Adding "User profile" service

---
+++ src/main/java/com/app/diamondhotelbackend/service/UserProfileService.java
@@ -0,0 +1,45 @@
+package com.app.diamondhotelbackend.service;
+
+import com.app.diamondhotelbackend.dto.RegisterRequestDto;
+import com.app.diamondhotelbackend.entity.UserProfile;
+import com.app.diamondhotelbackend.exception.UserProfileNotFoundException;
+import com.app.diamondhotelbackend.repository.UserProfileRepository;
+import com.app.diamondhotelbackend.util.Constant;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class UserProfileService {
+
+    private final UserProfileRepository userProfileRepository;
+
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserProfile getUserProfileByEmail(String email) {
+        return userProfileRepository.findUserProfileByEmail(email).orElseThrow(() -> new UserProfileNotFoundException(Constant.USER_PROFILE_NOT_FOUND));
+    }
+
+    public UserProfile saveUserProfile(RegisterRequestDto registerRequestDto) {
+        UserProfile userProfile = UserProfile.builder()
+                .email(registerRequestDto.getEmail())
+                .password(passwordEncoder.encode(registerRequestDto.getPassword()))
+                .firstname(registerRequestDto.getFirstname())
+                .lastname(registerRequestDto.getLastname())
+                .age(registerRequestDto.getAge())
+                .country(registerRequestDto.getCountry())
+                .passportNumber(registerRequestDto.getPassportNumber())
+                .phoneNumber(registerRequestDto.getPhoneNumber())
+                .city(registerRequestDto.getCity())
+                .street(registerRequestDto.getStreet())
+                .postalCode(registerRequestDto.getPostalCode())
+                .role(Constant.USER)
+                .build();
+
+        return userProfileRepository.save(userProfile);
+    }
+}

953b3b3749f1e635300c47c047c4a040792c4a19
szymonaszek01 <235879@edu.p.lodz.pl>
2023-06-02 23:29:15 +0200

    task-041 Adding new exception "Reservation not found"

---
+++ src/main/java/com/app/diamondhotelbackend/exception/ReservationNotFoundException.java
@@ -0,0 +1,5 @@
+package com.app.diamondhotelbackend.exception;
+
+public class ReservationNotFoundException extends RuntimeException{
+    public ReservationNotFoundException(String message) { super(message); }
+}

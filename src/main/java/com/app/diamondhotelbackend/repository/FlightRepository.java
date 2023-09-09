86003b399b9c5c0ecd8c86ff0282a254c9dd73a1
szymonaszek01 <235879@edu.p.lodz.pl>
2023-09-09 15:27:06 +0200

    task-058 Updating repositories

--- src/main/java/com/app/diamondhotelbackend/repository/FlightRepository.java
+++ src/main/java/com/app/diamondhotelbackend/repository/FlightRepository.java
@@ -4,10 +4,6 @@ import com.app.diamondhotelbackend.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-
-    Optional<Flight> findFlightByFlightNumber(String flightNumber);
 }

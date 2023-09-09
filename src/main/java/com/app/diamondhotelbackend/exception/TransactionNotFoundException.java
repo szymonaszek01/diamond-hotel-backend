6e886df48bf340118d3d4657a836e929f311895e
szymonaszek01 <235879@edu.p.lodz.pl>
2023-09-09 15:24:57 +0200

    task-058 Removing unused data transfer objects

--- src/main/java/com/app/diamondhotelbackend/exception/TransactionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.app.diamondhotelbackend.exception;
-
-public class TransactionNotFoundException extends RuntimeException {
-
-    public TransactionNotFoundException(String message) {
-        super(message);
-    }
-}

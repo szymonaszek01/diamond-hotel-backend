ef88ea986edbaeaee9d2387c23b14d815cd45182
szymonaszek01 <235879@edu.p.lodz.pl>
2023-09-17 14:51:30 +0200

    task-059 Updating "Date validation" and "Dates difference" in DateUtil

--- src/main/java/com/app/diamondhotelbackend/util/DateUtil.java
+++ src/main/java/com/app/diamondhotelbackend/util/DateUtil.java
@@ -1,26 +1,24 @@
 package com.app.diamondhotelbackend.util;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.sql.Date;
 import java.util.Optional;
 
 public class DateUtil {
 
-    public static Optional<LocalDateTime> isValidCheckInOrCheckOut(String localDateTimeAsString) {
+    public static Optional<Date> parseDate(String dateAsString) {
         try {
-            if (localDateTimeAsString == null || localDateTimeAsString.isEmpty()) {
+            if (dateAsString == null || dateAsString.isEmpty()) {
                 return Optional.empty();
             }
-            LocalDateTime localDateTime = LocalDateTime.parse(localDateTimeAsString, DateTimeFormatter.ISO_DATE_TIME);
-            return Optional.of(localDateTime);
 
-        } catch (NumberFormatException e) {
+            return Optional.of(Date.valueOf(dateAsString));
+
+        } catch (IllegalArgumentException e) {
             return Optional.empty();
         }
     }
 
-    public static long getDuration(LocalDateTime checkIn, LocalDateTime checkOut) {
-        return Duration.between(checkIn, checkOut).toDays();
+    public static long getDifferenceBetweenTwoDates(Date firstDate, Date secondDate) {
+        return Math.round((double) Math.abs(secondDate.getTime() - firstDate.getTime()) / (1000 * 60 * 60 * 24));
     }
 }

3a03ace01252056f3bd4c0e778aaeba617f25e5d
szymonaszek01 <235879@edu.p.lodz.pl>
2023-09-19 22:38:09 +0200

    task-060 Exposing api to handle "Booking room process"

---
+++ src/main/java/com/app/diamondhotelbackend/controller/ReservedRoomController.java
@@ -0,0 +1,22 @@
+package com.app.diamondhotelbackend.controller;
+
+import com.app.diamondhotelbackend.entity.ReservedRoom;
+import com.app.diamondhotelbackend.service.reservedroom.ReservedRoomServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/reserved-room")
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
+public class ReservedRoomController {
+
+    private final ReservedRoomServiceImpl reservedRoomService;
+
+    @GetMapping("/all/reservation/id/{id}")
+    public List<ReservedRoom> getReservedRoomListByReservationId(@PathVariable long id) {
+        return reservedRoomService.getReservedRoomListByReservationId(id);
+    }
+}

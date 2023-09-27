ff87ec46853f4a85318e8921a73b1cbabc04b4f3
szymonaszek01 <235879@edu.p.lodz.pl>
2023-09-27 21:37:42 +0200

    task-062 Adding "Thymeleaf" and creating templates for emails and pdf documents

--- src/main/java/com/app/diamondhotelbackend/security/config/ApplicationConfiguration.java
+++ src/main/java/com/app/diamondhotelbackend/security/config/ApplicationConfiguration.java
@@ -16,7 +16,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
@@ -25,6 +29,25 @@ import java.util.List;
 public class ApplicationConfiguration {
 
     private final UserProfileServiceImpl userProfileService;
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.addTemplateResolver(emailTemplateResolver());
+        return springTemplateEngine;
+    }
+
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+
+        emailTemplateResolver.setPrefix("/templates/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        emailTemplateResolver.setCacheable(false);
+
+        return emailTemplateResolver;
+    }
 
     @Bean
     public UserDetailsService userDetailsService() throws UsernameNotFoundException {

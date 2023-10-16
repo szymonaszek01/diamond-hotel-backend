0c8514e512572c4649e2f8e13e8760ef13df3b72
szymonaszek01 <235879@edu.p.lodz.pl>
2023-10-16 20:50:43 +0200

    task-067 Updating "Room" entity, adding paths, which are responsible for returning "Payment" or "Reserved Room" list

--- src/main/java/com/app/diamondhotelbackend/entity/Room.java
+++ src/main/java/com/app/diamondhotelbackend/entity/Room.java
@@ -1,6 +1,7 @@
 package com.app.diamondhotelbackend.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -23,6 +24,7 @@ public class Room {
     private int floor;
 
     @ManyToOne()
+    @JsonProperty("room_type")
     private RoomType roomType;
 
     @JsonIgnore
